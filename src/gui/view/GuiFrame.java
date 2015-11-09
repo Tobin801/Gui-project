9440e2c69999a1dc42a4a1ec57dc14abf8c463c3
Tobin801 <tand6053@740s-pl-103.local>
2015-11-09 12:51:17 -0700

    Added GuiPanel

--- src/gui/view/GuiFrame.java
+++ src/gui/view/GuiFrame.java
@@ -3,12 +3,29 @@ package gui.view;
 import javax.swing.JFrame;
 import gui.controller.GuiAppController;
 
+/**
+ * Creates the Frame for the Gui project.
+ * @author tand6053
+ * @version ) 0.3 Nov 9, 2015 Added a reference to the GuiPanl and installed it as the content pane.
+ */
+
 public class GuiFrame extends JFrame
 {
 	private GuiAppController baseController;
+	private GuiPanel basePanel;
 	
 	public GuiFrame(GuiAppController baseController)
 	{
-		this.baseController = baseController; //Assign parameter to data member.
+		this.baseController = baseController;   //Assign parameter to data member.
+		basePanel = new GuiPanel(baseController);   //Creates an instance of the GuiPanel with a reference to the controller.
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(basePanel);    //Sets the panel in the GuiFrame,this is REQUIRED!!!!!!!!
+		this.setResizable(false);		   //Usually a good idea but not required.
+		this.setSize(1280,720);            //Sets the size of the screen.
+		this.setVisible(true);             //Must be the last line of code in the setupFrame method.
 	}
 }
