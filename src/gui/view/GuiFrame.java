282a3768fd37a8f457a5d9d5df57d56d92646ef2
Tobin801 <tand6053@740s-pl-103.local>
2015-11-09 12:32:27 -0700

    Added GuiFrame

--- src/gui/view/GuiFrame.java
+++ src/gui/view/GuiFrame.java
@@ -1,6 +1,14 @@
 package gui.view;
 
-public class GuiFrame
-{
+import javax.swing.JFrame;
+import gui.controller.GuiAppController;
 
+public class GuiFrame extends JFrame
+{
+	private GuiAppController baseController;
+	
+	public GuiFrame(GuiAppController baseController)
+	{
+		this.baseController = baseController; //Assign parameter to data member.
+	}
 }
